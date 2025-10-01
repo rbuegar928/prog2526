b67567384d4afd77510c1508a849d22a60726722
Raúl Bueno <rbuegar928@g.educaand.es>
2025-10-01 12:36:41 +0200

    Actualización de tareas

---
+++ Ejercicio16.Clasificación.py
@@ -0,0 +1,14 @@
+nota = int(input("Intruduzca su nota: "))
+match nota:
+    case 0|1|2|3|4:
+        print("Insuficiente")
+    case 5:
+        print("Suficiente")
+    case 6:
+        print("Bien")
+    case 7|8:
+        print("Notable")
+    case 9|10:
+        print("Sobresaliente")
+    case _:
+        print("Ha ocurrido un error")
--- Ejercicio17.calculadora_basic.py
+++ Ejercicio17.calculadora_basic.py
@@ -1,17 +1,17 @@
-operación = input("Indique la operación que quiere realizar: ")
-if operación == "suma" :
+operación = input("Indique la operación que quiere realizar\n+ para suma\n- para resta\n* para multiplicación\n/ para división: ")
+if operación == "+" :
     num1 = int(input("Introduzca el primer sumando: "))
     num2 = int(input("Introduzca el segundo sumando: "))
     print("La suma es igual a ", num1 + num2)
-elif operación == "resta" :
+elif operación == "-" :
     num1 = int(input("Introduzca el primer número: "))
     num2 = int(input("Introduzca el número que quieres restar: "))
     print("La resta es igual a ", num1 - num2)
-elif operación == "multiplicación" :
+elif operación == "*" :
     num1 = int(input("Introduzca el primer número: "))
     num2 = int(input("Introduzca el número por el que lo quieres multiplicar: "))
     print("La multiplicación es igual a ", num1 * num2)
-elif operación == "división" :
+elif operación == "/" :
     num1 = int(input("Introduzca el dividendo: "))
     num2 = int(input("Introduzca el divisor: "))
     print("La división es igual a ", num1 / num2)
