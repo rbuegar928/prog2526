c41dc4c73035360036d3ef49651f57740214fc1e
Raúl Bueno <rbuegar928@g.educaand.es>
2025-10-06 13:27:03 +0200

    Nuevos ejercicios

---
+++ Tema 1/Ejercicio19.Hola_mundo_100veces.py
@@ -0,0 +1,5 @@
+# Escribir Hola mundo 100 veces
+i = 0
+while i < 101:
+  print(str(i)+"."+"Hola mundo!") # Le ponemos str para convertir el numero de i en una cadena
+  i += 1 # Para sumar 1 a la i 
