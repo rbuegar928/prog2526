6ac89cae4581a2901e0ece208a35e6af376e7698
Raúl Bueno <rbuegar928@g.educaand.es>
2025-10-08 12:47:12 +0200

    Ejercicio nuevo + cambio de nombre

--- Tema 1/Ejercicio26_Sumar_num_0_100.py
+++ Tema 1/Ejercicio26_Sumar_num_0_100.py
@@ -1,7 +1,6 @@
 # Calcualar la suma de los números del 1 al 100
-i = 0
+
 suma = 0
-while i <= 100:
+for i in range(0,101):
     suma = suma + i
-    i = i + 1
 print("El resultado es ", suma)
---
+++ Tema 1/Ejercicio27_Suma_num_100_0.py
@@ -0,0 +1,5 @@
+# Calcualar la suma de los números del 1 al 100 a la inversa
+suma = 0
+for i in range(100,0):
+    suma = suma + i
+print("La suma es igual a", suma)
