85eb3ac1eb7c6a4fa1d37c2001f41cbfbe59f56e
Raúl Bueno <rbuegar928@g.educaand.es>
2025-10-09 08:53:41 +0200

    Edición del último ejercicio

--- Tema 1/Ejercicio29_Calcular_factorial.py
+++ Tema 1/Ejercicio29_Calcular_factorial.py
@@ -1,9 +1,7 @@
 # Calcular el factorial de un número
 
 n = int(input("Introduce el número al que le quieres calcular el factorial: "))
-
-for i in range(0,n):
-    fact = n*i
-    i = i - 1
+fact = 1
+for i in range(1,n+1):
     fact = fact*i
 print("El factorial de", n, "es", fact)
