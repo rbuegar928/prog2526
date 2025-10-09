daaec16306f4831cb91745b3696f6aeed3d5a8ae
Raúl Bueno <rbuegar928@g.educaand.es>
2025-10-09 09:27:59 +0200

    Subida de un nuevo ejercicio

---
+++ Tema 1/Ejercicio30_funcion_par_impar.py
@@ -0,0 +1,11 @@
+# Función que deuvuelva si el número leído es par o impar
+
+def parimpar(N1):
+    if N1 % 2 == 0:
+        return("El número introducido es par.")
+    else :
+        return("El número introducido es impar.")
+
+N1 = int(input("Introduzca un número: "))
+
+print(parimpar(N1))
