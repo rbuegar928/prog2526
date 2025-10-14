f18aca8ec0ab5961526f7e535295d95cd50e5e58
Raúl Bueno <rbuegar928@g.educaand.es>
2025-10-14 14:48:11 +0200

    Subida de los últimos ejercicios

--- Tema 1/Ejercicio34_contrar_num_de_a.py
+++ Tema 1/Ejercicio34_contrar_num_de_a.py
@@ -1,9 +1,15 @@
-# Contar númerode "a" en una frase
+# Contar número de vocales en una frase y cambiarlas por X
 
-# Definir función
-def cuentaA(texto):
-    cuenta = texto.count("a") + texto.count("A")
-    return cuenta
-# Invocar función
-frase = input("Introduzca una frase prar contar su número de a: ")
-print(cuentaA(frase))
+# Definir cuenta
+def cuentaVocales(texto):
+    cuenta = texto.count("a") + texto.count("A") + texto.count("e") + texto.count("E") + texto.count("i") + texto.count("I") + texto.count("o") + texto.count("O") + texto.count("u") + texto.count("U")
+    print("El número de vocales que hay es", cuenta)
+# Definir función cuenta y reemplaza
+def cuentaYReemplaza(texto):
+    cuenta = texto.count("a") + texto.count("A") + texto.count("e") + texto.count("E") + texto.count("i") + texto.count("I") + texto.count("o") + texto.count("O") + texto.count("u") + texto.count("U")
+    print("El número de vocales que hay es", cuenta)
+    nuevoTexto = texto.replace("a", "x").replace("A", "X").replace("e", "x").replace("E", "X").replace("i", "x").replace("I", "X").replace("o", "x").replace("O", "X").replace("u", "x").replace("U", "X")
+    return nuevoTexto
+# Invocar funciones
+texto = input("Introduzca una frase prar contar su número de a: ")
+print(cuentaYReemplaza(texto))
