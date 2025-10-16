2db1c8c51797a324072a52e3081f2bf40fd9e858
Raúl Bueno <rbuegar928@g.educaand.es>
2025-10-16 09:53:51 +0200

    Subida del último ejercicio

--- Tema 1/Ejercicio36_calculadora_mejorada.py
+++ Tema 1/Ejercicio36_calculadora_mejorada.py
@@ -1,7 +1,7 @@
 #Calculadora basica
-import match
+import math
 operación = input("Indique la operación que quiere realizar\n+ para suma\n- para resta\n* para multiplicación\n/ para división\n root para raíz cuadrada\n power para potencia: ")
-num1 = input("Escriba un número: ")
+num1 = int(input("Escriba un número: "))
 if operación == "+" :
     num2 = int(input("Introduzca el segundo sumando: "))
     resultado = (num1 + num2)
@@ -14,11 +14,11 @@ elif operación == "*" :
 elif operación == "/" :
     num2 = int(input("Introduzca el divisor: "))
     resultado = (num1 / num2)
-elif operación = root:
-    resultado = match.sqrt(num1)
-elif operación = power:
-    num2 = ("Introduzca a cuánto quiere elevar el número: ")
-    resultado = pow(num1,
+elif operación == "root":
+    resultado = math.sqrt(num1)
+elif operación == "power":
+    num2 = int(input("Introduzca a cuánto quiere elevar el número: "))
+    resultado = math.pow(num1, num2)
 else :
     print("Operación no válida.")
 print("El resultado es", resultado)
