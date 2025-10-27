38a06d950ee6561b2b56637e2e6e9264613cae93
Raúl Bueno <rbuegar928@g.educaand.es>
2025-10-27 13:21:45 +0100

    Edición print array

--- Tema 2/Ejercicio2_1blucle_range_array_5_num.py
+++ Tema 2/Ejercicio2_1blucle_range_array_5_num.py
@@ -3,5 +3,5 @@ suma = 0
 lista = [21, 7, 92, 56, 208]
 for elemento in range(len(lista)):
     suma = suma + lista[elemento]
-    print ("La suma con el elemento actual,", elemento, "=", suma)
+    print ("La suma con el elemento actual,", lista[elemento], "=", suma)
 print("La suma final de todos los valores es:", suma)
