a663613d9a9b97adea3688100c431f914a9ff8af
rbuegar928 <rbuegar928@g.educaand.es>
2025-10-29 12:26:06 +0100

    Update Ejercicio4_sueldos_10%.py

--- Tema 2/Ejercicio4_sueldos_10%.py
+++ Tema 2/Ejercicio4_sueldos_10%.py
@@ -1,6 +1,6 @@
 # Sueldos con aumento del 10%
 sueldos = [1800, 1200, 2000, 1200, 900]
-
-for i in range(len(sueldos)):
+print(sueldos)
+for i in range(len(sueldos)): # recorremos todo el array
     sueldos[i] = (sueldos[i]*0.1) + sueldos[i]
 print(sueldos)
