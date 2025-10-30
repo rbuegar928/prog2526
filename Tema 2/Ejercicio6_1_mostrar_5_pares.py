fd22763bd50994d18fee408a4b6b1af2d21aab6a
Raúl Bueno <rbuegar928@g.educaand.es>
2025-10-30 10:32:03 +0100

    Subida de ejercicio

--- Tema 2/Ejercicio6_1_mostrar_5_pares.py
+++ Tema 2/Ejercicio6_1_mostrar_5_pares.py
@@ -1,4 +1,6 @@
 pares = [0]*5
+
+#Este método es usando if
 posicion = 0 # para cambiar los números dentro del array
 contador = 0 # contador de los números pares que llevamos
 i = 1 # son los números que prueba
@@ -11,3 +13,7 @@ while contador < 5:
     else:
         i = i + 1
 print(pares)
+
+#para hacerlo sin usar if aunque tampoco usamos array
+for i in range (2,11,2):
+    print(i)
---
+++ Tema 2/Ejercicio7_devolver_mayor_t.py
@@ -0,0 +1,11 @@
+def maximo (t):
+ for i in t:
+
+    
+return mayor
+
+
+a = [5,3,2,8,1]
+# Invocamos la función
+num_mayor = maximo(a)
+print (num_mayor)
