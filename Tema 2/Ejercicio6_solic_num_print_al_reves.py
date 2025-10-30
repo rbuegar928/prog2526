14d75511861a9901d78d80acb0e214e6c4aa8034
Raúl Bueno <rbuegar928@g.educaand.es>
2025-10-30 09:17:25 +0100

    Edición del último ejercicio

--- Tema 2/Ejercicio6_solic_num_print_al_reves.py
+++ Tema 2/Ejercicio6_solic_num_print_al_reves.py
@@ -3,5 +3,8 @@ cantidad = int(input("Introduce la cantidad de números que desee introducir: ")
 num =[]
 for i in range (cantidad):
     num.append(int(input("Introduzca un número entero: ")))
-num.reverse()
-print(num)
+#num.reverse()
+# con el reverse está ya hecho pero lo queremos hacer nosotros
+print("\nLos números en orden inverso son: ")
+for i in range(len(num)-1,-1,-1):
+    print(num[i])
