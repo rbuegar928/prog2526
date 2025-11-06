c527358caf69a6926d66a2a2a68f0523177276d1
Raúl Bueno <rbuegar928@g.educaand.es>
2025-11-06 10:25:00 +0100

    Nuevos ejercicios

---
+++ Tema 2/Ejercicio15_método_buscar_num.py
@@ -0,0 +1,19 @@
+def buscarNum (lista, n):
+    i = 0
+    respuesta = -1
+    while i < len(lista) and respuesta == -1:
+        if lista[i] == n:
+            respuesta = (f"El número {n} está en la posición {i}")
+        i = i + 1 
+    return(respuesta)
+
+
+import random
+
+num = []
+for i in range(5):
+    numero = random.randint(0, 10)  # Generar un número entre 0 y 10
+    num.append(numero)
+
+n = int(input("Introduzca el número que quiere buscar (entre 0 y 10): "))
+print(buscarNum(num,n))
